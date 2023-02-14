0a692bcf98939c7160f2d4711dc3ea228a9b37a7
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2023-02-13 17:45:34 -0900

    feat: sample message for Python TTS

--- src/communication.py
+++ src/communication.py
@@ -5,16 +5,41 @@
 
 """
 Source file for function project_description.
-Demonstrates morse code in Python.
+Demonstrates morse code (https://en.wikipedia.org/wiki/Morse_code) in Python.
 """
 
 import morse_talk as mtalk
+import pyttsx3
 
 
 def project_description(input):
     """First attempt at morse code in Python."""
     sample = mtalk.encode(input)
     print(f"Example program run: {sample}")
+
+
+def reverse_morse():
+    """Reverse morse code."""
+    command_mode = True
+
+    while command_mode:
+        command = input("Enter morse code: ")
+        if command == "exit":
+            command_mode = False
+        else:
+            print(mtalk.decode(command))
+
+def tts():
+    """Text to speech."""
+    # ?: Add text to speech.
+    sample_text = "This is a samle of text-to-speech running with Python."
+    pyttsx3.speak(sample_text)
+
+
+def switch_input():
+    """Connection betweeen keyboard input and morse code output."""
+    # ?: Add keyboard input.
+    pass
 
 
 if __name__ == "__main__":
@@ -25,5 +50,7 @@ if __name__ == "__main__":
         "Assistive Technology",
     ]
 
-    # TODO: Specify case-sensitive input?
-    project_description(message[2])
+    # project_description(message[0])
+    # reverse_morse()
+    tts()
+
