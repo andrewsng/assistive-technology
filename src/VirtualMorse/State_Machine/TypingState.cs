4938b4bc1f168ca175f1f11cab65abfb3d6bd8a0
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-19 22:39:12 -0800

    added base code for writing state

--- src/VirtualMorse/State_Machine/TypingState.cs
+++ src/VirtualMorse/State_Machine/TypingState.cs
@@ -10,11 +10,61 @@ public class TypingState : State
 
 	public override void dot()
     {
-		Console.WriteLine("*");
-    }
+		Console.WriteLine("storing dot");
+		stateMachine.addDot();
+		Console.WriteLine();
+	}
 
 	public override void dash()
     {
-		Console.WriteLine("-");
+		Console.WriteLine("storing dash");
+		Console.WriteLine("if TTT is entered: read entire page and clear TTT");
+		stateMachine.addDash();
+		Console.WriteLine();
+	}
+
+    public override void space()
+    {
+		Console.WriteLine("read current word: " + stateMachine.getCurrentWord());
+		Console.WriteLine("add current word to page");
+		Console.WriteLine("if no current word, add space and say 'Space'");
+		stateMachine.clearWord();
+		Console.WriteLine();
+	}
+
+    public override void shift()
+    {
+		Console.WriteLine("toggle capitalization");
+		stateMachine.setIsCapitalized(true);
+		Console.WriteLine("say shift");
+		Console.WriteLine();
+	}
+
+    public override void enter()
+    {
+		Console.WriteLine("move to validate");
+		//stateMachine.setState(stateMachine.getValidateState());
+		stateMachine.addLetterToWord();
+		Console.WriteLine();
+	}
+
+    public override void backspace()
+    {
+		Console.WriteLine("move to Backspace");
+		//stateMachine.setState(stateMachine.getBackspaceState());
+		Console.WriteLine();
+	}
+
+    public override void save()
+    {
+		Console.WriteLine("save text doc as is");
+		Console.WriteLine("says 'now saving'");
+		Console.WriteLine();
+    }
+
+    public override void command()
+    {
+		Console.WriteLine("move to command state");
+		//stateMachine.setState(stateMachine.getCommandState());
     }
 }
