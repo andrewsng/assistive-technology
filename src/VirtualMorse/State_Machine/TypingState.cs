c8c09f31334e76643b68f2732f47f42cfaebc0f3
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-19 21:53:01 -0800

    initial commit for TypingState class

---
+++ src/VirtualMorse/State_Machine/TypingState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class TypingState : State
+{
+	StateMachine stateMachine;
+	public TypingState(StateMachine stateMachine)
+	{
+		this.stateMachine = stateMachine;
+	}
+
+	public override void dot()
+    {
+		Console.WriteLine("*");
+    }
+
+	public override void dash()
+    {
+		Console.WriteLine("-");
+    }
+}
