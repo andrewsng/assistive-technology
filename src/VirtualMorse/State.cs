b17ce692e9ec53534a159d5e4af15cfd408ff7da
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-19 20:58:15 -0800

    initial creation of State class

---
+++ src/VirtualMorse/State.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class State
+{
+	public State()
+	{
+
+	}
+
+	public void Shift()
+    {
+
+    }
+
+	public void Command()
+    {
+
+    }
+
+	public void Save()
+    {
+
+    }
+
+    public void Dot()
+    {
+
+    }
+
+    public void Dash()
+    {
+
+    }
+
+    public void Enter()
+    {
+
+    }
+
+    public void Backspace()
+    {
+
+    }
+}
