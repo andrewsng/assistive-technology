3db4987148887661342549c48d15e00c5c42a309
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-19 21:33:05 -0800

    initial commit for StateMachine class

---
+++ src/VirtualMorse/State_Machine/StateMachine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class StateMachine
+{
+	State typingState;
+	State validateState;
+	State backspaceState;
+	State commandState;
+
+	String current_letter;
+	String current_word;
+	public StateMachine()
+	{
+	}
+}
