fc2e72c403bb4664aa34a24248fb49b425551f4b
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-19 22:38:53 -0800

    added base code for writing state

--- src/VirtualMorse/State_Machine/StateMachine.cs
+++ src/VirtualMorse/State_Machine/StateMachine.cs
@@ -9,8 +9,9 @@ public class StateMachine
 
 	State state;
 
-	String current_letter;
-	String current_word;
+	string current_letter;
+	string current_word;
+	bool is_Capitalized = false;
 	public StateMachine()
 	{
 		typingState = new TypingState(this);
@@ -19,6 +20,7 @@ public class StateMachine
 		state = typingState;
 	}
 
+	//Initialize States
 	public void dot()
     {
 		state.dot();
@@ -27,5 +29,109 @@ public class StateMachine
 	public void dash()
     {
 		state.dash();
+    }
+
+	public void space()
+    {
+		state.space();
+    }
+
+	public void shift()
+    {
+		state.shift();
+    }
+
+	public void enter()
+    {
+		state.enter();
+    }
+
+	public void backspace()
+    {
+		state.backspace();
+    }
+
+	public void save()
+    {
+		state.save();
+    }
+
+	public void command()
+    {
+		state.command();
+    }
+
+	//fetching / setting functions for states
+	public void setState(State state)
+    {
+		this.state = state;
+    }
+
+	public State getState()
+    {
+		return state;
+    }
+
+	public State getTypingState()
+    {
+		return typingState;
+    }
+
+	public State getValidateState()
+    {
+		return validateState;
+    }
+
+	public State getBackspaceState()
+    {
+		return backspaceState;
+    }
+
+	public State getCommandState()
+    {
+		return commandState;
+    }
+
+
+	//retrival / setting functions for member variables
+	public string getCurrentLetter()
+    {
+		return current_letter;
+    }
+	public string getCurrentWord()
+	{
+		return current_word;
+	}
+
+	public void setIsCapitalized(bool boolean)
+    {
+		is_Capitalized = boolean;
+    }
+
+
+	//helper functions
+	public void addDot()
+    {
+		current_letter += '*';
+    }
+
+	public void addDash()
+    {
+		current_letter += '-';
+    }
+
+	public void clearLetter()
+    {
+		current_letter = "";
+    }
+
+	public void addLetterToWord()
+    {
+		current_word += current_letter + " ";
+		current_letter = "";
+    }
+	public void clearWord()
+    {
+		current_word = "";
     }
 }
