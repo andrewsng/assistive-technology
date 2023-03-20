51c5451e904992623724292b454bc87929082a4a
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-19 21:32:52 -0800

    initial commit for State class

---
+++ src/VirtualMorse/State_Machine/State.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class State
+{
+	public State()
+	{
+	}
+
+	public void shift()
+    {
+
+    }
+
+	public void command()
+    {
+
+    }
+
+	public void save()
+    {
+
+    }
+
+    public void space() 
+    {  
+    
+    }
+
+    public void dot()
+    {
+
+    }
+
+    public void dash()
+    {
+
+    }
+
+    public void enter()
+    {
+
+    }
+
+    public void backspace()
+    {
+
+    }
+
+}
