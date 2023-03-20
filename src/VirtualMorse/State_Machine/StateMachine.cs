59d29134f4cfbf9bf9a597a4fb1dc1b0cced15ed
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-19 21:53:33 -0800

    added inheritance

--- src/VirtualMorse/State_Machine/StateMachine.cs
+++ src/VirtualMorse/State_Machine/StateMachine.cs
@@ -7,9 +7,25 @@ public class StateMachine
 	State backspaceState;
 	State commandState;
 
+	State state;
+
 	String current_letter;
 	String current_word;
 	public StateMachine()
 	{
+		typingState = new TypingState(this);
+
+		//set initial state
+		state = typingState;
 	}
+
+	public void dot()
+    {
+		state.dot();
+    }
+
+	public void dash()
+    {
+		state.dash();
+    }
 }
