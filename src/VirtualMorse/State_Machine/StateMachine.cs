2297d00d042b8ad81a1d78a6bc00fa93aba39e1a
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-24 08:30:01 -0800

    added extra functionality

--- src/VirtualMorse/State_Machine/StateMachine.cs
+++ src/VirtualMorse/State_Machine/StateMachine.cs
@@ -3,15 +3,11 @@
 public class StateMachine
 {
 	State typingState;
-	State validateState;
-	State backspaceState;
 	State commandState;
 
 	State state;
 
-	string current_letter;
-	string current_word;
-	bool is_Capitalized = false;
+	
 	public StateMachine()
 	{
 		typingState = new TypingState(this);
@@ -77,61 +73,26 @@ public class StateMachine
 		return typingState;
     }
 
-	public State getValidateState()
-    {
-		return validateState;
-    }
-
-	public State getBackspaceState()
-    {
-		return backspaceState;
-    }
-
 	public State getCommandState()
     {
 		return commandState;
     }
 
-
-	//retrival / setting functions for member variables
-	public string getCurrentLetter()
-    {
-		return current_letter;
-    }
-	public string getCurrentWord()
+	public  string getCurrentWord()
 	{
-		return current_word;
+		return state.getCurrentWord();
 	}
 
-	public void setIsCapitalized(bool boolean)
-    {
-		is_Capitalized = boolean;
-    }
+	public  string getCurrentLetter()
+	{
+		return state.getCurrentLetter();
+	}
+
+	public  string getFile()
+	{
+		return state.getFile();
+	}
 
 
-	//helper functions
-	public void addDot()
-    {
-		current_letter += '*';
-    }
 
-	public void addDash()
-    {
-		current_letter += '-';
-    }
-
-	public void clearLetter()
-    {
-		current_letter = "";
-    }
-
-	public void addLetterToWord()
-    {
-		current_word += current_letter + " ";
-		current_letter = "";
-    }
-	public void clearWord()
-    {
-		current_word = "";
-    }
 }
