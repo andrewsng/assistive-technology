a8cd78ff37ccacb6d3cecb77ceae45e84f80a318
Jacob Jakiemiec <jjakiemiec@gmail.com>
2023-03-23 19:20:12 -0800

    initial commit of the Function class

---
+++ src/VirtualMorse/Function.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Function
+{
+    static Dictionary<string, string> morse_map = new Dictionary<string, string>() {
+    {".-", "a"},
+    {"-...", "b"},
+    {"-.-.", "c"},
+    {"-..", "d"},
+    {".", "e"},
+    {"..-.", "f"},
+    {"--.", "g"},
+    {"....", "h"},
+    {"..", "i"},
+    {".---", "j"},
+    {"-.-", "k"},
+    {".-..", "l"},
+    {"--", "m"},
+    {"-.", "n"},
+    {"---", "o"},
+    {".--.", "p"},
+    {"--.-", "q"},
+    {".-.", "r"},
+    {"...", "s"},
+    {"-", "t"},
+    {"..-", "u"},
+    {"...-", "v"},
+    {".--", "w"},
+    {"-..-", "x"},
+    {"-.--", "y"},
+    {"--..", "z"}
+    };
+
+    public static string morseToText(string morse)
+    {
+        if (morse_map.ContainsKey(morse))
+            return morse_map[morse];
+        else
+            return "";
+    }
+}
