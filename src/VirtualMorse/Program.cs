a2d66066d70c6427ae8b6330ada265775f1447ef
Andrew Ng <ngandrew1337@gmail.com>
2023-03-24 22:35:54 -0800

    Connect state machine functions to switch inputs

--- src/VirtualMorse/Program.cs
+++ src/VirtualMorse/Program.cs
@@ -11,18 +11,16 @@ namespace VirtualMorse
 {
     public class ProgramForm : Form
     {
+        public StateMachine stateMachine = new StateMachine();
         public FunctionKeyInput fKeys = new FunctionKeyInput();
         public InputHandler handler = new InputHandler();
         public RichTextBox textBox;
         static String versionStr = "Virtual Morse 2023";
 
-        private bool commandMode = false;
-
         public ProgramForm()
         {
             fKeys.KeyPressed += handler.DeviceInputReceived;
             handler.InputReceived += Handler_InputReceived;
-
 
             textBox = new RichTextBox();
             textBox.Dock = DockStyle.Fill;
@@ -41,48 +39,39 @@ namespace VirtualMorse
         {
             Switch input = e.switchInput;
             Console.WriteLine($"Received input in form - {input}");
-            if (input == Switch.Switch1 ||  input == Switch.Switch9)
+            switch (input)
             {
-                commandMode = true;
-            }
-            else if (input == Switch.Switch2)
-            {
-                if (commandMode)
-                {
-                    // PRINT PAGE
-                    commandMode = false;
-                }
-                else
-                {
-                    // SHIFT
-                }
-            }
-            else if (input == Switch.Switch3)
-            {
-                if (commandMode)
-                {
-                    // CLEAR DOCUMENT
-                    commandMode = false;
-                }
-                else
-                {
-                    // SAVE
-                }
-            }
-            else if (input == Switch.Switch4)
-            {
-                // SPACE & ADD WORD
-            }
-            else if (input == Switch.Switch5)
-            {
+                // COMMAND MODE
+                case Switch.Switch1:  // Fall through
+                case Switch.Switch9:
+                    stateMachine.command();
+                    break;
+
+                // SHIFT [PRINT PAGE]
+                case Switch.Switch2:
+                    stateMachine.shift();
+                    break;
+
+                // SAVE [CLEAR DOCUMENT]
+                case Switch.Switch3:
+                    stateMachine.save();
+                    break;
+
+                // SPACE (ADD WORD)
+                case Switch.Switch4:
+                    stateMachine.space();
+                    break;
+
                 // DOT
-            }
-            else if (input == Switch.Switch6)
-            {
+                case Switch.Switch5:
+                    stateMachine.dot();
+                    break;
+
                 // DASH
-            }
-            else if (input == Switch.Switch7)
-            {
+                case Switch.Switch6:
+                    stateMachine.dash();
+                    break;
+
                 // ENTER LETTER
                 //
                 // If Command Mode:
@@ -94,68 +83,29 @@ namespace VirtualMorse
                 //   'y' -> Replies to email. (Not working in current)
                 //   'n' -> Adds email address nickname.
                 //   'a' -> Ties email address to nickname.
-            }
-            else if (input == Switch.Switch8)
-            {
+                case Switch.Switch7:
+                    stateMachine.enter();
+                    break;
+
                 // BACKSPACE
+                case Switch.Switch8:
+                    stateMachine.backspace();
+                    break;
             }
+            Console.WriteLine("current letter: '" + stateMachine.getCurrentLetter() + "'");
+            Console.WriteLine("current word: '" + stateMachine.getCurrentWord() + "'");
+            Console.WriteLine("current document: '" + stateMachine.getFile() + "'");
         }
 
         [STAThread]
         public static void Main(string[] args)
         {
-            /*
             SpeechSynthesizer synth = new SpeechSynthesizer();
             synth.SetOutputToDefaultAudioDevice();
             synth.Speak(versionStr);
 
             Application.EnableVisualStyles();
             Application.Run(new ProgramForm());
-            */
-            StateMachine stateMachine = new StateMachine();
-
-            stateMachine.dot();
-            stateMachine.dash();
-            stateMachine.enter();
-            Console.WriteLine();
-
-            stateMachine.dot();
-            stateMachine.enter();
-            Console.WriteLine();
-
-            stateMachine.dash();
-            stateMachine.dash();
-            stateMachine.dash();
-            stateMachine.shift();
-            stateMachine.shift();
-            stateMachine.shift();
-            stateMachine.enter();
-            Console.WriteLine();
-
-            stateMachine.backspace();
-            Console.WriteLine("current letter: '" + stateMachine.getCurrentLetter() + "'");
-            Console.WriteLine("current word: '" + stateMachine.getCurrentWord() + "'");
-            Console.WriteLine("current document: '" + stateMachine.getFile() + "'");
-
-
-            stateMachine.space();
-            stateMachine.space();
-            Console.WriteLine();
-
-            stateMachine.dash();
-            stateMachine.dash();
-            stateMachine.dash();
-            stateMachine.enter();
-            stateMachine.space();
-            stateMachine.space();
-            Console.WriteLine();
-
-            Console.WriteLine();
-            Console.WriteLine("current letter: '" + stateMachine.getCurrentLetter() + "'");
-            Console.WriteLine("current word: '" + stateMachine.getCurrentWord() + "'");
-            Console.WriteLine("current document: '" + stateMachine.getFile() + "'");
-            Console.ReadLine();
-
         }
     }
 }
